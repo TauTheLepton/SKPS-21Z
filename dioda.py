ebddab040c196b1121bfd2dd8a28ee717f6203d1
Mateusz Palczuk <mpalczuk@lew.robotyka.ia.pw.edu.pl>
2021-12-16 16:03:35 +0100

    ^Co tam byczq???

--- dioda.py
+++ dioda.py
@@ -1,13 +1,13 @@
 # do ratunkowego, ale ie testowalismy
-import RPi.GPIO as GPIO
-import time
-GPIO.setmode(GPIO.BCM)
-GPIO.setuo(16, GPIO.OUT)
-while 1:
-	GPIO.output(16, 1)
-	time.sleep(1)
-	GPIO.output(16, 0)
-	time.sleep(1)
+# import RPi.GPIO as GPIO
+# import time
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setuo(16, GPIO.OUT)
+# while 1:
+# 	GPIO.output(16, 1)
+# 	time.sleep(1)
+# 	GPIO.output(16, 0)
+# 	time.sleep(1)
 
 # open wrt
 
@@ -16,7 +16,7 @@ import gpiod
 import time
 
 chip = gpiod.Chip('gpiochip0')
-prt = chip.get_lines([16])
+prt = chip.get_lines([40])
 prt.request(consumer='foobar', type=gpiod.LINE_REQ_DIR_OUT, default_vals=[0])
 
 for i in range(10):
@@ -26,19 +26,19 @@ for i in range(10):
 	time.sleep(1)
 
 # dzialajace gpioled2
-import gpiod
-import time
-import math
+# import gpiod
+# import time
+# import math
 
-chip = gpiod.Chip('gpiochip0')
-prt = chip.get_lines([16])
-prt.request(consumer='foobar', type=gpiod.LINE_REQ_DIR_OUT, default_vals=[0])
+# chip = gpiod.Chip('gpiochip0')
+# prt = chip.get_lines([16])
+# prt.request(consumer='foobar', type=gpiod.LINE_REQ_DIR_OUT, default_vals=[0])
 
-tmp = 1/10**3
-x = 0
-while x<20:
-	prt.set_values([0])
-	time.sleep((math.sin(x)+1)/2*tmp)
-	prt.set_values([1])
-	time.sleep((1 - (math.sin(x)+1)/2)*tmp)
-	x += tmp*2
+# tmp = 1/10**3
+# x = 0
+# while x<20:
+# 	prt.set_values([0])
+# 	time.sleep((math.sin(x)+1)/2*tmp)
+# 	prt.set_values([1])
+# 	time.sleep((1 - (math.sin(x)+1)/2)*tmp)
+# 	x += tmp*2
